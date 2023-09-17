6b98865ceea2b118339cea0fe2b3b1cfa8c63f11
Varun Chadha <vichadha@protonmail.com>
2023-09-17 15:18:20 -0700

    added references to ui controller

---
+++ Assets/Scripts/UIControllers/LevelSelectUIController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectUIController : MonoBehaviour
+{
+    [SerializeField] GameObject levelMenu;
+
+    [SerializeField] Image levelPreviewImage;
+    [SerializeField] Image levelTitle;
+    [SerializeField] List<Image> levelTime;
+    [SerializeField] Image levelTokens;
+
+    [SerializeField] List<Image> stars;
+
+    [SerializeField] Image starTotal;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
