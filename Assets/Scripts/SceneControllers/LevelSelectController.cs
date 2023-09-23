01451ec53d3cbb491ab2e8836c5611b1e6ce4569
Varun Chadha <vichadha@protonmail.com>
2023-09-23 15:28:49 -0700

    Minor fixes with enemy movement and level progession

--- Assets/Scripts/SceneControllers/LevelSelectController.cs
+++ Assets/Scripts/SceneControllers/LevelSelectController.cs
@@ -227,11 +227,6 @@ public class LevelSelectController : ISceneController
 
         if (!selectedLevel.level.IsHomeBase())
         {
-            Debug.Log($"{selectedLevel.level.GetLevelTitle()}");
-            foreach (bool starData in selectedLevel.levelData.starData)
-            {
-                Debug.Log(starData);
-            }
             UpdateLevelMenu();
         }
     }
@@ -259,7 +254,7 @@ public class LevelSelectController : ISceneController
         {
             // If we are locked check if we can unlock
             // TODO: move this unlock level check to level data
-            if (levelList[i].levelData.isLocked && levelList[i].level.CanUnlockLevel(collectedStars))
+            if (levelList[i].levelData.isLocked && levelList[i].level.CanUnlockLevel(collectedStars) && levelList[i - 1].levelData.IsStarUnlocked(LevelData.StarTypes.LEVEL_COMPLETE))
             {
                 levelList[i].level.UnlockLevel();
                 levelList[i].levelData.isLocked = false;
